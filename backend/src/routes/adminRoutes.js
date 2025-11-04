4d4969b4a6b8b5872df1cce4c4593609d613d19d
TejaatExploring <bhanuteja279@gmail.com>
2025-11-04 22:19:29 +0530

    Initial setup

--- backend/src/routes/adminRoutes.js
+++
@@ -1,8 +0,0 @@
-﻿import express from "express";
-import { getUsers, updateRole } from "../controllers/adminController.js";
-import { adminMiddleware } from "../middleware/auth.js";
-
-const router = express.Router();
-router.get("/users", adminMiddleware, getUsers);
-router.patch("/users/:id/role", adminMiddleware, updateRole);
-export default router;
